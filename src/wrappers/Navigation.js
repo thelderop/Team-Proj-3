df6cbc57fd62adef46964dc004b9931b98ca045b
jimmygranadino <jimmy.granadino@gmail.com>
2020-07-25 17:23:32 -0700

    foundation of base components setup

--- src/wrappers/Navigation.js
+++ src/wrappers/Navigation.js
@@ -4,14 +4,14 @@ import { NavLink, Link, Switch } from 'react-router-dom'
 const Navigation = (props) => {
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#"><strong>EVENT TRACKER</strong></a>
+            <a class="navbar-brand" href="#"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarColor01">
                 <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
+                <li class="nav-item">
                     <NavLink className="nav-link" exact to="/">Welcome</NavLink>
                 </li>
                 <li class="nav-item">
@@ -22,6 +22,9 @@ const Navigation = (props) => {
                 </li>
                 <li class="nav-item">
                     <NavLink className="nav-link" to="/login">Login</NavLink>
+                </li>
+                <li class="nav-item">
+                    <NavLink className="nav-link" to="/profile">Profile</NavLink>
                 </li>
                 </ul>
 
