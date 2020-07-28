b8ed672d3f51c7b98864b2167b57a63ac9f39f69
Louis <lreillywillhight@gmail.com>
2020-07-27 21:17:37 -0700

    added some comments to backend/routes/v1/favorites.js to help frontend better understand how CRUD calls work

--- backend/routes/v1/favorites.js
+++ backend/routes/v1/favorites.js
@@ -2,22 +2,33 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models')
 
+////////////// ROUTES
+
 //Get Favorites
+//Lists all of collection 'user' from database, as an array of JSON objects
+
+//tells browser to execute this function on GET /routes/v1/favorites.js
 router.get('/', (req, res) => {
+    //query database, db.Favorite is exported from models/index.js 
     db.Favorite.find()
-        .then(favorites => {
+    // after backend waits for api(db) response, do the next thing (anonymous promise function) (favorites is the query returned from db)
+      .then(favorites =>
+            //accept response as favorites, send to frontend
         res.send(favorites)
     })
     .catch(err => console.log(err))
 })
 
+//simple test route for debugging, no response expected from db
 router.get('/test', function (req, res) {
     console.log(`successfully connected to backend!!!!!!!! favorites.js`)
 })
 
-// GET inevidial event
-router.get('/:id', (req, res) => {
-    db.Event.findById(req.params.id)
+
+// GET inevidial event, currently searches by parameter :id, having multiple calls of single type(get, post, ...) to a single location ('/') is problematic; (now /view/:id)
+router.get('/view/:id', (req, res) => {
+    //
+    db.Favorite.findById(req.params.id)
         .then((favorite) => {
             res.send(favorite)
         }).catch(err => console.log('err in GET request favorite.js'))
@@ -26,12 +37,16 @@ router.get('/:id', (req, res) => {
 
 // CREATE favorite within database
 router.post('/addFave', (req,res) => {
+    //doing this prevents an error in findOne query argument
     let eventTitle = req.body.title
     db.Favorite.findOne({ title: eventTitle})
     .then(favorite => {
+        //if favorite searched for is found, return error
         if (favorite) {
             return console.log('error, favorite already exists in collection')
+            //else insert() in db
         } else {
+            //I'd like to figure out how to clean this up to be more readable.
             const newFavorite = new Favorite({
                 title: req.body.title,
                 location: req.body.location,
@@ -39,7 +54,9 @@ router.post('/addFave', (req,res) => {
                 description: req.body.description,
                 // userId: req.body.userId
             })
+            //creates object, I may try to pass req.body here
             newFavorite.save()
+            //sets response to new json object - I'm not entirely sure how this works.
             .then(fave => res.json(fave))
         }
     })
@@ -49,10 +66,14 @@ router.post('/addFave', (req,res) => {
 //GET UPDATE
 //Stretch
 router.put('/updateFaveByName/:name', (req, res) => {
+    //currently searches by req.params, this can be simpler with req.body
     db.Favorite.findOneAndUpdate(
+        // find the document
         { title: req.params.name },
+        //define new document content (currently overwrites all values)
         { title: req.body.title, location: req.body.location, description: req.body.description, date: req.body.date}
     )
+        //promise function to be run after db sends response
         .then(updatedFavorite => {
         res.send(updatedFavorite)
     })
@@ -62,10 +83,14 @@ router.put('/updateFaveByName/:name', (req, res) => {
 //GET DELETE
 //stretch
 router.delete('/deleteFaveById/:id', (req, res) => {
-    db.Event.findOneAndDelete({ _id: req.params.id }).then(deleteFavorite => {
-        console.log(deleteFavorite)
-        res.send({ Message: 'Event Deleted' }).catch(err => { console.error(err) })
+    db.Event.findOneAndDelete(
+        //document to be deleted
+        { _id: req.params.id }
+        )
+        .then(deleteFavorite => {
+        res.send({ Message: 'Event Deleted' })
     })
+    .catch(err => {console.error(err)})
 })
 
 module.exports = router
