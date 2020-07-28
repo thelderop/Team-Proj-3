a9e6d5a10261e174b8beea85cde4d6e0910b7656
jimmygranadino <jimmy.granadino@gmail.com>
2020-07-28 16:07:10 -0700

    cleaning up some bootstrap+linking

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,11 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom';
+import EventsDisplay from './EventsDisplay'
+import axios from 'axios';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
 
 export default function Favorites(props) {
 
+    // test array of objects to mimic API response
+    const testEvents = [{
+        "url": "http://sandiego.eventful.com/events/lgbt-book-club-/E0-001-134699507-9?utm_source=apis&utm_medium=apim&utm_campaign=apic",
+        "id": "E0-001-134699507-9",
+        "city_name": "San Diego"
+    }]
+
+    //calls API on page render
+    useEffect(() => {
+        axios.get(`https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?app_key=NFRS6FwLVhcNKTWD&keywords=concerts&location=Seattle&date=Future`)
+        .then(response => {
+            console.log(`${process.env.EVENTFUL_KEY}`)
+            setEvents(response.data.events.event)
+            console.log(response.data.events.event)
+        })
+        .catch(err => console.log('ERROR IN frontend /components/Calendar.js: '+JSON.stringify(err)))
+    }, [])
+
+    //array of objects, iterated on in EventsDisplay.js
+    const [events,setEvents] = useState(testEvents)
+
+    // let eventNames = {
+    //     events.map(event => {
+    //         <div>
+    //             <p>{event.name}</p>
+    //             <br/>
+    //         </div>
+    //     })
+    // }
+
     return (
-        <div>
-            <h1>Favorite page</h1>
+        <div className="Calendar">
+            <div class="row margin">
+                <div class="col-lg-4">
+                    <h3 id="calendar-title">Upcoming Events</h3>
+                    <div id="events-display-container">
+                        {/* <EventsDisplay events={events} /> */}
+                    </div>
+                </div>
+                <div class="col-lg-8 error">
+                    <FullCalendar
+                    plugins={[ dayGridPlugin ]}
+                    initialView="dayGridMonth"
+                    />
+                </div>
+            </div>
+            {/* <p>{JSON.stringify(events)} </p> */}
         </div>
     )
 }
+
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,18 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Welcome() {
     return (
-    <div className="row margin">
-        <div className="col-lg-2"></div>
-        <div className="col-lg-8">
-            <div className="jumbotron">
+    <div className="row">
+        <div className="col-lg-8 offset-lg-2">
+        <div className="jumbotron my-3">
             <h1 className="display-3">EVENT TRACKER</h1>
             <p className="lead">Events anywhere and everywhere! Event Tracker allows you to save the ones that pique your interest and find it on a map.</p>
             <hr className="my-2" />
             <p>Never suffer from FOMO again!</p>
-            <p className="lead">
-                <a className="btn btn-dark btn-lg" href="/register" role="button">Sign up!</a>
-            </p>
+            <NavLink className="btn btn-dark btn-lg" to="/register" role="button">Sign Up!</NavLink>
         </div>
         </div>
     </div>
