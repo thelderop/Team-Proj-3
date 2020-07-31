3a97ab4649bf4f67541656463f83f1fca891d4e3
jimmygranadino <jimmy.granadino@gmail.com>
2020-07-31 09:32:14 -0700

    diverged app

--- src/components/EventsDisplay.js
+++ src/components/EventsDisplay.js
@@ -39,6 +39,20 @@ const EventsDisplay = (props) => {
         })
     }
 
+    let handleRemove = (e) => {
+        let removeFavorite = {
+            email: user.email,
+            value: eventful.id
+        }
+        axios.post(`${process.env.REACT_APP_API}/v1/users/remove`, removeFavorite)
+        .then (res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
     // let handleAction = `${process.env.REACT_APP_API}/v1/users/add`
 
 
@@ -50,13 +64,17 @@ const EventsDisplay = (props) => {
             <div class="card-header">{eventful.start_time}</div>
             <div class="card-body">
                 <h4 class="card-title">{eventful.title}</h4>
-                <p class="card-text">{eventful.id}</p>
                 <p class="card-text">{eventful.venue_address}</p>
                 <p class="card-text">{desc}</p>
                 <form method="PUT" encType="application/x-www-form-urlencoded" onSubmit={handleAdd} >
-                    <input hidden type="email" name="email" email={user.email} onChangle={handleEmail}/>
-                    <input hidden type="text" name="value" value={eventful.id} onChangle={handleValue}/>
+                    <input hidden type="email" name="email" email={user.email} onChange={handleEmail}/>
+                    <input hidden type="text" name="value" value={eventful.id} onChange={handleValue}/>
                     <button type="submit" class="btn btn-info">Add to Favorites</button>
+                </form>
+                <form method="PUT" encType="application/x-www-form-urlencoded" onSubmit={handleRemove} >
+                    <input hidden type="email" name="email" email={user.email} onChange={handleEmail}/>
+                    <input hidden type="text" name="value" value={eventful.id} onChange={handleValue}/>
+                    <button type="submit" class="btn btn-info">Remove from Favorites</button>
                 </form>
             </div>
         </div>
